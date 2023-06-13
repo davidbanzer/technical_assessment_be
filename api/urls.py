c31c52b3666d1b6df0b5071e5356fd25dc129e33
David <davidbanzer123@gmail.com>
2023-06-13 10:02:11 -0400

    Update api route

--- api/urls.py
+++ api/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 
 
 urlpatterns = [
-    path('string-problem/', StringsProblemView.as_view()),
+    path('strings-problem/', StringsProblemView.as_view()),
     path('chess-problem/', ChessProblemView.as_view()),
 ]
